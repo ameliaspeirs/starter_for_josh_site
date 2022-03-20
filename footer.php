a6fc52cb4c2b4a50b578dfff5d960b5b6d7ed919
ameliaspeirs <amelia.janet.speirs@gmail.com>
2022-03-20 13:09:58 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+<footer class="container-fluid">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<p>JOSH WHITKIN</p>
+
+
+
+</div>
+<p> Terms and Conditions</p>
+
+
+</div> <!---row-->
+
+</div> <!---container-->
+</footer>
+
+<?php wp_footer(); ?>
+
+
+</body>
+</html>
