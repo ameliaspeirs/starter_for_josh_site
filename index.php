b454787e9a35c4080267844c9167fd8a34c2d8ac
ameliaspeirs <amelia.janet.speirs@gmail.com>
2022-03-20 13:09:50 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php get_header();?> <!--- Tells WordPress to include in the header .php --->
+<section class="container-fluid aboutbg text-center">
+<div class="container">
+<h2> GAMES &amp; LEVEL DESIGNER</h2>
+<p class="particle"> ...... and particle wizard</p>
+<P class="about-text"> My  name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
+    I always tell a story.</P>
+<div class="blocker"></div>
+</div> <!---container-->
+<?php wp_head(); ?> <!---very important that I added this-->
+</section>
+
+
+<section class="container-fluid">
+<div class="container"> 
+<h3 class="text-center"> LATEST ARTICILES</h3>
+<div class="row">
+
+
+</div class="col-md-4">
+<img class="img-fluid" src="Images/port1.jpg" alt="woman">
+<h4>A PORTRAIT</h4>
+<p> Friday November 2030</p>
+<p> STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+</p>
+  <a href="#"  CONTINUE READING ></a>
+
+</div class="col-md-4">
+<img src="Images/port2.jpg" alt="woman">
+<h4 class="article-title">A PORTRAIT</h4>
+<p class="date"> Friday November 2030</p>
+<p class="article-p"> STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+</p>
+  <a class="readmore" href="#"  CONTINUE READING ></a>
+
+</div class="col-md-4">
+<img src="Images/port3.jpg" alt="woman">
+<h4>A PORTRAIT</h4>
+<p> Friday November 2030</p>
+<p> STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+</p>
+  <a href="#"  CONTINUE READING ></a>
+   
+</div> <!---container-->
+
+</section>
+
+<footer class="container-fluid">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<p>JOSH WHITKIN</p>
+
+
+
+</div>
+<p> Terms and Conditions</p>
+
+
+</div> <!---row-->
+
+</div> <!---container-->
+</footer>
+<?php get_fotter();?> <!-- Tells wordpress to include footer .php -->
